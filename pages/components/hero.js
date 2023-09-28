5df83af38a6c856b898695267b672a7e88387d05
MaisarahMohamed <maisarahm00@gmail.com>
2023-09-28 22:52:50 +0800

    finished home page interface screen paint

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -1,6 +1,29 @@
+import {BiRightArrow } from "react-icons/bi";
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 const Hero = () => {
     return ( 
-        <section className="hero-container"></section>
+        <section className="hero-container">
+            <div className="hero-content">
+                <div className="hero-play-container">
+                    <div className="progress-bar">
+                        <CircularProgressbarWithChildren value={75} counterClockwise={'true'} strokeWidth={3} styles={buildStyles({trailColor: 'transparent',pathColor: '#fed530'})}>
+                            <div className="play-btn">
+                                <BiRightArrow style={{fontSize:'36px'}}/>
+                            </div>
+                        </CircularProgressbarWithChildren>
+                        
+                    </div>
+                </div>
+                <div className="hero-right-content">
+                    <h1>Find your movies here!</h1>
+                    <p style={{paddingTop:'30px',width:'75%'}}>
+                        Explore our gallery full of exciting films from all around the globe only your yor entertainments. No hidden charges or disturbing ads.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 }
  
--- pages/components/new-release.js
+++ pages/components/new-release.js
@@ -1,9 +1,56 @@
+import { BiTimeFive } from "react-icons/bi";
+import { AiOutlineEye } from "react-icons/ai";
+
 const NewRelease = () => {
     return ( 
-        <section className="content-container">
-
-        </section>
-     );
+    <section className="content-container">
+        <div className="movie-container">
+            <div className="space-between" style={{alignItems:'center',paddingBottom:'50px'}}>
+                <h2 className="font-medium">New Releases</h2>
+                <h5 style={{color:'#fed530'}}>View More</h5>
+            </div>
+            <div className="grid-container">
+            <div className="card">
+                <div className="card-content">
+                    <div className="card-genre">
+                        Fantasy
+                    </div>
+                    <div>
+                        <div className="duration-view">
+                            <div style={{paddingRight:'30px'}}><BiTimeFive style={{paddingRight:'10px', fontSize:'28px',verticalAlign:'middle'}}/>
+                                1hr 2min
+                            </div>
+                            <div>
+                                <AiOutlineEye style={{paddingRight:'10px', fontSize:'28px',verticalAlign:'middle'}}/>
+                                100K
+                            </div>
+                        </div>
+                        <h2 className="font-medium">Movie Title</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-content">
+                    <div className="card-genre">
+                        Fantasy
+                    </div>
+                    <div>
+                        <div className="duration-view">
+                            <div style={{paddingRight:'30px'}}><BiTimeFive style={{paddingRight:'10px', fontSize:'28px',verticalAlign:'middle'}}/>
+                                1hr 2min
+                            </div>
+                            <div>
+                                <AiOutlineEye style={{paddingRight:'10px', fontSize:'28px',verticalAlign:'middle'}}/>
+                                100K
+                            </div>
+                        </div>
+                        <h2 className="font-medium">Movie Title</h2>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+    );
 }
- 
 export default NewRelease;
